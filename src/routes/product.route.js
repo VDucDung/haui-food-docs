a633897edcb0ad6fd39eef4c2df322ae24fdb3bb
VDucDung <dunga3qv2@gmail.com>
2024-04-13 23:20:59 +0700

    ✨ Create docs api create a product

---
+++ src/routes/product.route.js
@@ -0,0 +1,60 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: Product management and retrieval
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a product
+ *     description: Only admins or shops can create products.
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - description
+ *               - price
+ *               - image
+ *               - categoryId
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               image:
+ *                 type: string
+ *                 format: uri
+ *               categoryId:
+ *                 type: string
+ *                 format: objectId
+ *             example:
+ *               name: Bánh mì
+ *               description: Bánh mì cao cấp
+ *               price: 20000
+ *               image: https://hitly.vn/avatar-default
+ *               categoryId: 5ebac534954b54139806c112
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Product'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *
+ */
