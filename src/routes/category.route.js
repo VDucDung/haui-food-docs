f6c09a05f1b6f4ea3aaac1c604a85baa592d6594
VDucDung <dunga3qv2@gmail.com>
2024-04-14 17:50:57 +0700

    ⚡️ Create docs api create a category

---
+++ src/routes/category.route.js
@@ -0,0 +1,46 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Categories
+ *   description: Category management and retrieval
+ */
+
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Create a category
+ *     description: Only admins can create categories.
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - image 
+ *             properties:
+ *               name:
+ *                 type: string
+ *               image:
+ *                 type: string
+ *                 format: url
+ *             example:
+ *               name: Đồ ăn
+ *               image: https://hitly.vn/avatar-default
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Category'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
