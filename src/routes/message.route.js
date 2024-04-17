3b0c452eb241fc64fa1f8297c678720be8537f4c
VDucDung <dunga3qv2@gmail.com>
2024-04-18 01:26:18 +0700

    🚀 Write docs api for create a message

---
+++ src/routes/message.route.js
@@ -0,0 +1,50 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Messages
+ *   description: Message management and retrieval
+ */
+
+/**
+ * @swagger
+ * /messages:
+ *   post:
+ *     summary: Create a message
+ *     description: Users can create message.
+ *     tags: [Messages]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - senderId
+ *               - receiverId
+ *               - message
+ *             properties:
+ *               senderId:
+ *                 type: objectId
+ *               receiverId:
+ *                 type: objectId
+ *               message:
+ *                 type: string
+ *             example:
+ *               senderId: 6602f79a65bdc867e599b25e
+ *               receiverId: 65fd6f75b449440a709beaa7
+ *               message: Hello
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Message'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ * 
+ */
