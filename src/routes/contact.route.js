78dea6e453aaf4f7100930f12151b8289f04fc3d
VDucDung <dunga3qv2@gmail.com>
2024-04-19 22:43:57 +0700

    📝 Write docs api for get all contact

--- src/routes/contact.route.js
+++ src/routes/contact.route.js
@@ -48,4 +48,69 @@
  *           application/json:
  *             schema:
  *                $ref: '#/components/schemas/Contact'
+ * 
+ *   get:
+ *     summary: Get all contacts
+ *     description: Only admins can retrieve all contacts.
+ *     tags: [Contacts]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: Contact name
+ *       - in: query
+ *         name: role
+ *         schema:
+ *           type: string
+ *         description: Contact role
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *         description: sort by query in the form of field:desc/asc (ex. name:asc)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of users
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Contact'
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 1
+ *                 totalResults:
+ *                   type: integer
+ *                   example: 1
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
  */
